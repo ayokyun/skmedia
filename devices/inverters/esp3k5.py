5bfb9a6f5e083a87e9652bd494380399959a4aa0
cckn <cckn1985@gmail.com>
2018-10-12 11:25:57 +0900

    protocol define modify

--- devices/inverters/esp3k5.py
+++ devices/inverters/esp3k5.py
@@ -1,71 +1,151 @@
 from pprint import pprint
 import serial
-import struct
+import collections
 
 
-class Esp3k5(object):
-
-    queryFormat = [0x0a, 0x96, 0xFF, 0x54, 0x18, 0x05, 0xFF]
-
-    def __init__(self, stationId: int):
-        self.stationId: int = stationId
-        self.rawData = None
-        self.data = None
+class Helper:
 
     @staticmethod
-    def bytes2int(_bytes):
+    def bytes_to_int(_bytes):
         if type(_bytes) == bytearray or type(_bytes) == bytes:
             return int.from_bytes(_bytes, byteorder='little', signed=False)
         elif type(_bytes) == int:
             return _bytes
 
-    def request(self):
-        print("request :: 아직 미구현")
-        # raise NotImplementedError
+    @staticmethod
+    def calculate_checksum(data):
+        result = 0
+        for x in data:
+            result = result ^ x
 
-    def makeQuery(self):
-        ret = bytearray(self.queryFormat)
-        ret[2] = self.stationId
-        ret[6] = (ret[2] + ret[3] + ret[4]) % 256
-        return bytes(ret)
+        return result
 
-    def getRawData(self):
+    @staticmethod
+    def get_data_from_bytes(raw_data, protocol):
+        print(raw_data, protocol)
+        data = raw_data[protocol['idx']:protocol['idx'] + protocol['length']]
 
-        with serial.Serial('COM8', 9600, timeout=0.1) as ser:
-            ser.write(self.makeQuery())
-            line = ser.readline()  # read a '\n' terminated line
-            return line
+        data = int.from_bytes(data, byteorder='little', signed=False)
+        if protocol['devide']:
+            data /=  protocol['devide']
+        return data
 
-    def updateData(self):
-        pass
+
+class Esp3k5(object):
+
+    def __init__(self, station_id: int):
+        if station_id <= 0 or station_id > 99:
+            raise ValueError
+        self.station_id: int = station_id
+        self.data = None
+
+    def update_data(self, data):
+        # self.get_rawdata()
+        return True
         # return True  # 성공 시
 
         # return False  # 실패 시
 
-    def verifyRawData(self, data):
-        print("verifyResponse :: 아직 미구현")
+    def get_rawdata(self, port: str, baudRate: int, timeout=0.1):
+
+        with serial.Serial(port, baudRate, timeout=0.1) as ser:
+            ser.write(self.make_request())
+            line = ser.readline()  # read a '\n' terminated line
+            return line
+
+    def make_request(self):
+        protocol = Protocol
+        request_protocol = Protocol.request_protocol
+
+        ret = bytearray(protocol.request_format)
+        ret[request_protocol['stationId']['idx']] = self.station_id
+        ret[request_protocol['checkSum']['idx']] = (
+            ret[2] + ret[3] + ret[4]) % 256
+        return bytes(ret)
+
+    def verify_response(self, data):
+        # protocol = Protocol
+        # resIdx = protocol.response_index
+        res_proto = Protocol.response_protocol
+
+        # check Length
+        if len(data) != 32:
+            return False
+
+        # check Header
+        if (data[res_proto['header']['idx']] != 0xb1) \
+                or (data[res_proto['header']['idx']+1] != 0xb7):
+            return False
+
+        # check ID
+        if data[res_proto['stationId']['idx']] != self.station_id:
+            return False
+
+        # checkSum
+        if data[res_proto['checkSum']['idx']] != Helper.calculate_checksum(data[0:-1]):
+            return False
         return True
 
-    def parseResponse(self, data):
-        if self.verifyRawData(data):
-            rawData = dict()
-            rawData['solarVoltage1'] = self.bytes2int(data[3:5])
-            rawData['solarVoltage2'] = self.bytes2int(data[7:9])
-            rawData['solarCurrent'] = self.bytes2int(data[5:7])
-            rawData['lineVoltage'] = self.bytes2int(data[9:11])
-            rawData['lineCurrent'] = self.bytes2int(data[11:13])
-            rawData['temperature'] = self.bytes2int(data[13:15])
-            rawData['energyToday'] = self.bytes2int(data[15:16])
-            rawData['energyTotal'] = self.bytes2int(data[17:20])
-            rawData['faultCode'] = self.bytes2int(data[20:24])
-            rawData['runStatus'] = self.bytes2int(data[24])
-            pprint(rawData)
-            self.rawData = rawData
-        else:
-            print("vertify Error ")
+    def parse_response(self, data):
+        # protocol = Protocol
+        # resIdx = protocol.response_index
+
+        parsed_data = dict()
+        # parsed_data['solarVoltage1'] = 360.5
+        # # parsed_data['solarVoltage1'] = Helper.bytes_to_int((data[3:5]))
+        # parsed_data['solarCurrent'] = Helper.bytes_to_int(data[5:7])
+        # parsed_data['solarVoltage2'] = Helper.bytes_to_int(data[7:9])
+        # parsed_data['lineVoltage'] = Helper.bytes_to_int(data[9:11])
+        # parsed_data['lineCurrent'] = Helper.bytes_to_int(data[11:13])
+        # parsed_data['temperature'] = Helper.bytes_to_int(data[13:15])
+        # parsed_data['energyToday'] = Helper.bytes_to_int(data[15:16])
+        # parsed_data['energyTotal'] = Helper.bytes_to_int(data[17:20])
+        # parsed_data['faultCode'] = Helper.bytes_to_int(data[20:24])
+        # parsed_data['runStatus'] = Helper.bytes_to_int(data[24])
+        # parsed_data['frequency'] = Helper.bytes_to_int(
+        #     data[resIdx.frequency.idx: resIdx.frequency.idx+resIdx.frequency.length])
+        # parsed_data['operationTime'] = Helper.bytes_to_int(data[24])
+        # parsed_data['powerFactor'] = Helper.bytes_to_int(data[24])
+        # parsed_data['DSPVersion'] = Helper.bytes_to_int(data[24])
+        # # pprint(parsed_data)
+        self.data = parsed_data
+
+        return True
 
     def __del__(self):
         pass
+
+
+class Protocol:
+    di = collections.namedtuple("dataInfo", "idx length")
+    request_format = [0x0a, 0x96, 0xFF, 0x54, 0x18, 0x05, 0xFF]
+
+    request_protocol = {"header": {"idx": 0, "length": 2},
+                        "stationId": {"idx": 2, "length": 1},
+                        "command": {"idx": 3, "length": 1},
+                        "length": {"idx": 4, "length": 1},
+                        "tail": {"idx": 5, "length": 1},
+                        "checkSum": {"idx": 6, "length": 1},
+                        }
+
+    response_protocol = {"header": {"idx": 0, "length": 2},
+                         "stationId": {"idx": 2, "length": 1},
+                         "solarVoltage1": {"idx": 3, "length": 2, "devide": 10},
+                         "solarCurrent": {"idx": 5, "length": 2, "devide": 10},
+                         "solarVoltage2": {"idx": 7, "length": 2, "devide": 10},
+                         "lineVoltage": {"idx": 9, "length": 2, "devide": 10},
+                         "lineCurrent": {"idx": 11, "length": 2, "devide": 10},
+                         "temperature": {"idx": 13, "length": 2, "devide": 10},
+                         "energyToday": {"idx": 15, "length": 2, "devide": 100},
+                         "energyTotal": {"idx": 17, "length": 3},
+                         "faultCode": {"idx": 20, "length": 4},
+                         "runStatus": {"idx": 24, "length": 1},
+                         "frequency": {"idx": 25, "length": 2, "devide": 10},
+                         "operationTime": {"idx": 27, "length": 2},
+                         "powerFactor": {"idx": 29, "length": 1, "devide": 100},
+                         "dspVersion": {"idx": 30, "length": 1, "devide": 10},
+                         "checkSum": {"idx": 31, "length": 1}
+                         }
 
 
 if __name__ == '__main__':
@@ -75,5 +155,5 @@ if __name__ == '__main__':
     data = esp3k5_dummy.makeDummyData()
 
     esp = Esp3k5(1)
-    esp.request()  # not Implement
-    esp.parseResponse(data)
+    # esp.request()  # not Implement
+    esp.parse_response(data)
--- tests/devices/inverters/esp3k5_test.py
+++ tests/devices/inverters/esp3k5_test.py
@@ -1,64 +1,156 @@
 import pytest
-
 import unittest
+
 from devices.inverters import esp3k5
 
 
-class Esp3k5(unittest.TestCase):
+class HelperTest(unittest.TestCase):
+    helper = esp3k5.Helper
+    protocol = esp3k5.Protocol
+    rawData = bytearray([0xB1, 0xB7, 0x01, 0x15, 0x0E,
+                         0x05, 0x01, 0x01, 0x0E, 0x9A,
+                         0x08, 0xC8, 0x01, 0x60, 0x01,
+                         0xFF, 0x00, 0x9F, 0x86, 0x01,
+                         0x00, 0x00, 0x00, 0x00, 0x01,
+                         0x59, 0x02, 0x01, 0x01, 0x63,
+                         0x6E, 0x9D])
+
+    def test_calculate_checksum(self):
+        """ return type is bool """
+        ret = self.helper.calculate_checksum(self.rawData[:-1])
+        self.assertEqual(ret, 0x9d)
+
+    # @unittest.
+    def test_get_data_from_bytes(self):
+        """ get data from bytes """
+        protocol = self.protocol.response_protocol["solarVoltage1"]
+        ret = self.helper.get_data_from_bytes(self.rawData, protocol)
+        self.assertEqual(ret, 360.5)
+
+
+class Esp3k5Test(unittest.TestCase):
 
     def setUp(self):
-        self.stationId = 1
-        self.esp3k5 = esp3k5.Esp3k5(self.stationId)
+        self.station_id = 1
+        self.esp3k5 = esp3k5.Esp3k5(self.station_id)
 
     def tearDown(self):
         pass
 
-    def test_import_esp3k5(self):
-        """ 테스트 파일이 import 되었는지 확인 """
+    def test_is_instance_esp3k5(self):
+        """ self.esp3k5 is esp3k5.Esp3k5's Instance """
         self.assertIsInstance(self.esp3k5, esp3k5.Esp3k5)
 
-    def test_exist_make_query_function(self):
+    def test_station_id_range(self):
+        """station Id range is 0 ~ 99 """
+        with self.assertRaises(ValueError):
+            esp3k5.Esp3k5(-1)
+        with self.assertRaises(ValueError):
+            esp3k5.Esp3k5(100)
+
+        valid_station_id = 11
+        esp3k5_instance = esp3k5.Esp3k5(valid_station_id)
+        self.assertEqual(esp3k5_instance.station_id, valid_station_id)
+
+    def test_make_request_function(self):
         """ make query test """
-        result = self.esp3k5.makeQuery()
+        result = self.esp3k5.make_request()
 
         self.assertEqual(type(result), bytes)  # check Type
         self.assertEqual(len(result), 7)  # check Length
         assert result[0] == 0x0a and result[1] == 0x96  # check Header
-        assert result[2] == self.stationId  # Station Id
+        assert result[2] == self.station_id  # Station Id
         assert result[3] == 0x54  # command
         assert result[4] == 0x18  # length
         assert result[5] == 0x05  # tail
         assert result[6] == (result[2] + result[3] +
                              result[4]) % 256  # checkSum
 
-    def test_get_rawData(self):
-        ret = self.esp3k5.getRawData()
-        self.assertEqual(type(ret), bytes)  # check Type
-        self.assertEqual(len(ret), 32)  # check Length
 
-    def test_update_data(self):
-        self.esp3k5.updateData()
-
-
-class VerifyRawData(unittest.TestCase):
+# @unittest.skip("")
+class VerifyResponseTest(unittest.TestCase):
 
     def setUp(self):
-        self.stationId = 1
-        self.esp3k5 = esp3k5.Esp3k5(self.stationId)
+        self.station_id = 1
+        self.esp3k5 = esp3k5.Esp3k5(self.station_id)
         self.rawData = bytearray([0xB1, 0xB7, 0x01, 0x15, 0x0E,
                                   0x05, 0x01, 0x01, 0x0E, 0x9A,
                                   0x08, 0xC8, 0x01, 0x60, 0x01,
                                   0xFF, 0x00, 0x9F, 0x86, 0x01,
                                   0x00, 0x00, 0x00, 0x00, 0x01,
                                   0x59, 0x02, 0x01, 0x01, 0x63,
-                                  0x6E, 0x69])
+                                  0x6E, 0x9D])
 
     def test_return_type_is_bool(self):
-        returnValue = self.esp3k5.verifyRawData(self.rawData)
+        """ return type is bool """
+        returnValue = self.esp3k5.verify_response(self.rawData)
         self.assertEqual(type(returnValue), type(True))
 
     def test_check_header(self):
-        self.assertTrue(self.esp3k5.verifyRawData(bytearray(self.rawData)))
+        """ header is 0xb1b7 """
+        correctData = self.rawData
+        self.assertTrue(self.esp3k5.verify_response(correctData))
+
+        invalidData = self.rawData
+        invalidData[0] += 1
+        self.assertFalse(self.esp3k5.verify_response(invalidData))
+
+        invalidData = correctData
+        invalidData[1] -= 1
+        self.assertFalse(self.esp3k5.verify_response(invalidData))
+
+    def test_check_length(self):
+        """ length is 32 """
+        correctData = self.rawData
+        self.assertTrue(self.esp3k5.verify_response(correctData))
+
+        invalidData = correctData[:-1]  # remove last byte
+        self.assertFalse(self.esp3k5.verify_response(invalidData))
+
+    def test_station_id(self):
+        """ data[2] is stationid """
+        correctData = self.rawData
+        self.assertTrue(self.esp3k5.verify_response(correctData))
+
+        invalidData = self.rawData
+        invalidData[2] += 1
+        self.assertFalse(self.esp3k5.verify_response(invalidData))
+
+    def test_checksum(self):
+        """ checksum is data[0] ~ data[31] xor calculate 
+        (and process for overflow)  """
+        correctData = self.rawData
+        self.assertTrue(self.esp3k5.verify_response(correctData))
+
+        invalidData = self.rawData
+        invalidData[31] += 1
+        self.assertFalse(self.esp3k5.verify_response(invalidData))
+
+
+@unittest.skip("dd")
+class ParseDataTest(unittest.TestCase):
+
+    def setUp(self):
+        self.station_id = 1
+        self.esp3k5 = esp3k5.Esp3k5(self.station_id)
+        self.rawData = bytearray([0xB1, 0xB7, 0x01, 0x15, 0x0E,
+                                  0x05, 0x01, 0x01, 0x0E, 0x9A,
+                                  0x08, 0xC8, 0x01, 0x60, 0x01,
+                                  0xFF, 0x00, 0x9F, 0x86, 0x01,
+                                  0x00, 0x00, 0x00, 0x00, 0x01,
+                                  0x59, 0x02, 0x01, 0x01, 0x63,
+                                  0x6E, 0x9D])
+
+    def test_get_rawData(self):
+        ret = self.esp3k5.get_rawdata("COM8", 9600)
+        self.assertEqual(type(ret), bytes)  # check Type
+        self.assertEqual(len(ret), 32)  # check Length
+
+    def test_parse_response(self):
+
+        self.assertIsNotNone(self.esp3k5.parse_response(self.rawData))
+        self.assertIsNotNone(self.esp3k5.data)
+        self.assertEqual(self.esp3k5.data["solarVoltage1"], 360.5)
 
 
 if __name__ == '__main__':
